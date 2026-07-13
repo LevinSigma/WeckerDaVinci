e48d1c08025622e1439098b44f9b57f5282cc476
SigmaEssey <nico.hubler@bluewin.ch>
2026-07-13 15:37:40 +0200

    feat: weckerüberarbeitung

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import AlarmCard from './components/AlarmCard.jsx'
-import WeatherCard from './components/WeatherCard.jsx'
-
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-      <AlarmCard />
-      <WeatherCard />
+    <AlarmCard />
   </StrictMode>,
 )
